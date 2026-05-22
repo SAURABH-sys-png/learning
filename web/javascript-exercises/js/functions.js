ac2716f798daa04ae78b521cd89d22ceb7747fb3
SAURABH-sys-png <saurabhrathee95@gmail.com>
2026-05-22 20:35:35 +0530

    Did functions and rest things

---
+++ web/javascript-exercises/js/functions.js
@@ -0,0 +1,18 @@
+function addtwonums(num1,num2){
+    console.log(num1+num2);
+}
+
+
+addtwonums(2897346,23);
+
+function login(username) {
+    return `${username} just logged in`
+}
+
+const name = login("SAurabh");
+
+console.log(name);
+
+// undefined is considered false
+
+
---
+++ web/javascript-exercises/js/rest.js
@@ -0,0 +1,14 @@
+// when number of parameters are not known we use rest operator
+
+function printing(val1,val2,...nums){
+    console.log(nums);
+}
+
+printing(1,2,3,4,5,6,7,8);
+// it return ans array
+
+function print(num){
+    console.log(num);
+}
+
+print(90);
