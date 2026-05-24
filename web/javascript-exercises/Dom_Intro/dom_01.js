808eb578f640c7811a52da3585df4f83b80649e8
SAURABH-sys-png <saurabhrathee95@gmail.com>
2026-05-24 22:38:20 +0530

    Started with dom third time

---
+++ web/javascript-exercises/Dom_Intro/dom_01.js
@@ -0,0 +1,10 @@
+console.log(window.document)
+// is equivalent to saying console.log(document);
+
+console.log(document.links)
+// return a html collection of all the links inside of the complete document tree
+// properties kuch kuch arrays jsi hoti hai 
+// get element by id
+
+document.getElementById('some-id').innerHTML = "<h1>Saurabh</h>"
+
