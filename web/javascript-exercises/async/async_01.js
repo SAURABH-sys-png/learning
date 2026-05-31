f3762fac2661ce77d8fd3d5bc828fe7b34402a9f
SAURABH-sys-png <saurabhrathee95@gmail.com>
2026-05-31 22:23:30 +0530

    hc

---
+++ web/javascript-exercises/async/async_01.js
@@ -0,0 +1,13 @@
+// in general all codees are syncronous 
+// and single threaded means everything executes oon one single thread
+
+
+// jab tak purana process execute nhi hoga tab tak agla kaam nhi kar sakte 
+// call stack and memory heaps remember 
+
+// bLOCKING CODE AND NON BLOCKING CODE 
+// Blocking code will block the code of the complete program 
+// non blocking does not block any flow bas chalta rehta hai 
+// in general file reading causes blocking code 
+// so we use non blocking code that is asyncrous nature comes in 
+
