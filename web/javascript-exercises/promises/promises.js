2e89306d545d7cc6a333097a77c333fa5dfab592
SAURABH-sys-png <saurabhrathee95@gmail.com>
2026-06-03 21:54:17 +0530

    June 3

---
+++ web/javascript-exercises/promises/promises.js
@@ -0,0 +1,36 @@
+const mypromise = new Promise(function (resolve, reject) {
+  setTimeout(function () {
+    console.log("Complete");
+    resolve();
+  }, 1000);
+});
+
+mypromise.then(function () {
+  console.log("Promise consumed");
+});
+
+new Promise(function (resolve, reject) {
+  setTimeout(function () {
+    console.log("Async task 2");
+    resolve();
+  }, 3000);
+}).then(function () {
+  console.log("Async log 2");
+});
+
+const prom3 = new Promise(function (resolve, reject) {
+  setTimeout(function () {
+    resolve({ username: "Ch", email: "chai@example" });
+  }, 1000);
+});
+prom3.then(function (user) {
+  console.log(user);
+});
+
+
+async function consume() {
+    const response = await prom3
+    console.log(response);
+}
+
+consume()
