1219c38bd96582fcd25d6a3bb14a1e3dae722bc2
SAURABH-sys-png <saurabhrathee95@gmail.com>
2026-06-19 11:38:18 +0530

    June 19

--- st-pro/basic-chat/server/server.js
+++ st-pro/basic-chat/server/server.js
@@ -1,3 +1,18 @@
 import express from "express";
 import { WebSocketServer } from "ws";
 
+const app = express();
+
+const posts = [
+    {
+        username: 'kyle',
+        title: 'Post 1'
+    }
+]
+
+app.listen('/posts', (req,res) => {
+    res.json(posts)
+})
+
+
+app.listen(3000);
